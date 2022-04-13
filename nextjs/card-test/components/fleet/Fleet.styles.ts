5e890f2374a9e96866a751adbbd0ba9a5608d1d9
combustiblemon <comblmn@gmail.com>
2022-04-13 14:56:52 +0300

    created Fleet component

---
+++ nextjs/card-test/components/fleet/Fleet.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import Card from '../card/Card';
+
+export const Wrapper = styled(Card)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 10px;
+`;
---
+++ nextjs/card-test/components/fleet/Fleet.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Wrapper } from './Fleet.styles';
+
+const Fleet = () => {
+  return <Wrapper tooltip="hello" statusSize="small" />;
+};
+
+export default Fleet;
