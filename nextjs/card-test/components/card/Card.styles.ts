cbe337330b634bba5dd3788e46dc074c0c7eb035
combustiblemon <comblmn@gmail.com>
2022-04-13 12:04:29 +0300

    created card

---
+++ nextjs/card-test/components/card/Card.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { white } from '../../modules/colors';
+
+export const Wrapper = styled.div`
+  display: flex;
+
+  background-color: ${white};
+  padding: 10px;
+
+  border-radius: 8px;
+`;
---
+++ nextjs/card-test/components/card/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Wrapper } from './Card.styles';
+
+const Card = () => {
+  return <Wrapper>helloooo</Wrapper>;
+};
+
+export default Card;
