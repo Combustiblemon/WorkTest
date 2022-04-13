26cadc536d419c9caf4d337da11859eb881e200d
combustiblemon <comblmn@gmail.com>
2022-04-13 11:06:19 +0300

    copied flarmio configs

---
+++ nextjs/test-app/next.config.js
@@ -0,0 +1,22 @@
+const withTM = require('next-transpile-modules')([
+    '@mui/material',
+    '@mui/system',
+    '@mui/icons-material'
+  ]);
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      styledComponents: true
+    },
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@mui/styled-engine': '@mui/styled-engine-sc'
+      };
+      return config;
+    }
+  };
+
+  module.exports = withTM(nextConfig);
