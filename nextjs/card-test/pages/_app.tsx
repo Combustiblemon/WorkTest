d25e2149bd0d1fe3b393a42630a112dc2921111a
combustiblemon <comblmn@gmail.com>
2022-04-13 12:04:58 +0300

    style changes

--- nextjs/card-test/pages/_app.tsx
+++ nextjs/card-test/pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable import/no-mutable-exports */
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import type { AppProps } from 'next/app';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ nextjs/card-test/styles/index.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import { gray2 } from '../modules/colors';
+
+export const Wrapper = styled.div`
+  height: 600px;
+  width: 600px;
+
+  background-color: ${gray2};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
