90f99240ebacc98290a15e44d2cd00be8e6ac50a
combustiblemon <comblmn@gmail.com>
2022-04-13 14:15:44 +0300

    completed card

--- nextjs/card-test/components/card/Card.styles.ts
+++ nextjs/card-test/components/card/Card.styles.ts
@@ -1,12 +1,71 @@
+import { green, red } from '@mui/material/colors';
 import styled from 'styled-components';
 
-import { white } from '../../modules/colors';
+import { black, gray1, gray2, gray4, gray5, white } from '../../modules/colors';
 
-export const Wrapper = styled.div`
+const cardStyle = {
+  primary: {
+    backgroundColor: white,
+    color: gray1,
+    hoverColor: gray4,
+    activeColor: gray5,
+    border: 'none',
+  },
+};
+
+export const Wrapper = styled.div<{
+  border?: boolean;
+  shadow?: boolean;
+  disabled?: boolean;
+  type: 'primary';
+}>`
   display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 
-  background-color: ${white};
-  padding: 10px;
+  background-color: ${(props) => cardStyle[props.type].backgroundColor};
+  ${(props) =>
+    props.shadow && 'box-shadow: 0px 0px 3px 3px rgba(0, 0, 0, 0.5);'}
 
   border-radius: 8px;
+
+  ${(props) =>
+    props.disabled &&
+    `pointer-events: none;
+     background-color: ${gray2};
+    `}
+
+  :hover {
+    background-color: ${(props) => cardStyle[props.type].hoverColor};
+  }
+
+  :active {
+    background-color: ${(props) => cardStyle[props.type].activeColor};
+  }
+`;
+
+const statusStyle = {
+  size: {
+    big: 10,
+    small: 5,
+  },
+  color: {
+    rejected: red,
+    active: green,
+    disabled: black,
+  },
+};
+
+export const Status = styled.div<{
+  size: 'big' | 'small';
+  color: 'rejected' | 'active' | 'disabled';
+}>`
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  width: ${(props) => `${statusStyle.size[props.size]}px`};
+  background-color: ${(props) => `${statusStyle.color[props.color]}`};
+`;
+
+export const Content = styled.div`
+  padding: 10px;
 `;
--- nextjs/card-test/components/card/Card.tsx
+++ nextjs/card-test/components/card/Card.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import { Tooltip, Zoom } from '@mui/material';
+import React, { ReactNode } from 'react';
 
-import { Wrapper } from './Card.styles';
+import { Content, Status, Wrapper } from './Card.styles';
 
-const Card = () => {
-  return <Wrapper>helloooo</Wrapper>;
+export interface CardPropType {
+  border?: boolean;
+  children?: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  shadow?: boolean;
+  statusColor?: 'rejected' | 'active' | 'disabled';
+  statusSize?: 'big' | 'small';
+  tooltip?: string;
+  type?: 'primary';
+}
+
+const Card = ({
+  type = 'primary',
+  border,
+  children,
+  className,
+  disabled,
+  onClick,
+  shadow,
+  statusColor = 'disabled',
+  statusSize,
+  tooltip = ' ',
+}: CardPropType) => {
+  return (
+    <Tooltip
+      TransitionComponent={Zoom}
+      TransitionProps={{ timeout: 600 }}
+      title={tooltip}
+    >
+      <Wrapper
+        className={className}
+        type={type}
+        border={border}
+        shadow={shadow}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {statusSize && <Status size={statusSize} color={statusColor} />}
+        <Content>{children}</Content>
+      </Wrapper>
+    </Tooltip>
+  );
 };
 
 export default Card;
